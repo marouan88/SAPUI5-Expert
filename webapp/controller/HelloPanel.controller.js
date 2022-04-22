8de016fe179c05caf54eb62d450773dffeacb04b
Maro <marouanmesdouri@gmail.com>
2022-04-22 11:31:39 +0200

    7.3 Dialogo

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,13 +1,15 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast", 
+    "sap/ui/core/Fragment"
 ],
     /**
     * @param {typeof sap.ui.core.mvc.Controller} Controller
     * @param {typeof sap.m.MessageToast} MessageToast
+    * @param {typeof sap.ui.core.Fragment} Fragment
     */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
         return Controller.extend("logaligroup.sapui5.controller.HelloPanel", {
             onInit: function () {
@@ -19,6 +21,27 @@ sap.ui.define([
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
+            }, 
+
+            onShowDialog: function(){
+                const oView = this.getView();
+
+                if(!this.byId("helloDialog")){
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "logaligroup.sapui5.view.HelloDialog"
+                    }).then(function(oDialog){
+                        oView.addDependent(oDialog);
+                        oDialog.indexOfDependent();
+    
+                    })
+
+                }
+                else{
+                    this.byId("helloDialog").open();
+                }
+                
+                
             }
         });
     });
